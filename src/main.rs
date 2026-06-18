35f07993df0082278e1e4f4bee69db0793611a22
Fabian Gonzalez <fabiangonz98@gmail.com>
2026-06-17 20:57:34 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use std::{
-    io::Write,
+    io::{Read, Write},
     net::{TcpListener, TcpStream},
+    thread::spawn,
 };
 
-fn handle_connection(mut stream: TcpStream) -> std::io::Result<usize> {
-    let resp = "+PONG\r\n";
-    return stream.write(resp.as_bytes());
+fn handle_connection(mut stream: TcpStream) {
+    let mut req = [0; 1024];
+    while let Ok(bytes) = stream.read(&mut req) {
+        println!("Bytes read: {bytes}");
+        if bytes == 0 {
+            break;
+        }
+
+        stream.write(b"+PONG\r\n").unwrap();
+    }
 }
 
 fn main() {
@@ -13,14 +21,9 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => match handle_connection(_stream) {
-                Ok(bytes) => {
-                    println!("Bytes written: {bytes}");
-                }
-                Err(e) => {
-                    println!("error: {e}");
-                }
-            },
+            Ok(_stream) => {
+                spawn(move || handle_connection(_stream));
+            }
             Err(e) => {
                 println!("error: {}", e);
             }
