c51bf210ae2c8edcce9300d9c85c572de73fba30
Fabian Gonzalez <fabiangonz98@gmail.com>
2026-06-17 20:28:27 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
-#![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{
+    io::Write,
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_connection(mut stream: TcpStream) -> std::io::Result<usize> {
+    let resp = "+PONG\r\n";
+    return stream.write(resp.as_bytes());
+}
 
 fn main() {
-    // println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
+            Ok(_stream) => match handle_connection(_stream) {
+                Ok(bytes) => {
+                    println!("Bytes written: {bytes}");
+                }
+                Err(e) => {
+                    println!("error: {e}");
+                }
+            },
             Err(e) => {
                 println!("error: {}", e);
             }
